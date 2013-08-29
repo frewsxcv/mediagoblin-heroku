46317ec1a6eb3f752b4c4653a52d92b55462052f
Corey Farwell <coreyf@rwell.org>
2013-08-28 23:50:55 -0700

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# MediaGoblin-Heroku
+
+A project dedicated to making [MediaGoblin](http://mediagoblin.org) easily deployable to [Heroku](http://heroku.com).
+
+## Setup instructions
+
+Coming soon
---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from mediagoblin.app import MediaGoblinApp
+from configobj import ConfigObj
+
+
+CONFIG_FILE_PATH = "mediagoblin_local.ini"
+
+database_url = os.environ.get("DATABASE_URL", False)
+
+if database_url:
+    parser = ConfigObj(CONFIG_FILE_PATH)
+    parser["mediagoblin"]["sql_engine"] = database_url
+    parser.write(open(CONFIG_FILE_PATH, "w"))
+
+os.system("gmg dbupdate")
+
+app = MediaGoblinApp(CONFIG_FILE_PATH)
